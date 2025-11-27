39a266e45701e9c4ca2a0cedcf484078d04c8f76
arunima <arunima@sirpi.io>
2025-11-27 14:45:54 +0530

    REFACTOR:changed the theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,179 +1,195 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-const baseUrl = '/';
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-// remove the old footer_col_3 import since we'll inline everything
-// const foot = require('./static/footer_col_3');
+const baseUrl = "/";
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'User Manual',
-  tagline: 'Guide to navigating Forest Stack platform features, roles, and support resources.',
-  url: 'https://docs.forestdx.iudx.io/',
+  title: "User Manual",
+  tagline: "Guide to navigating Forest Stack platform features.",
+  url: "https://docs.forestdx.iudx.io/",
   baseUrl,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/forest_logo.svg',
-  organizationName: 'ForestDx', // Usually your GitHub org/user name.
-  projectName: 'Forest Stack', // Usually your repo name.
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "assets/new-forest/catalogue/forest_logo.svg",
+  organizationName: "ForestDx",
+  projectName: "Forest Stack",
 
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
-        blog: {
-          showReadingTime: true,
-          editUrl:
-            'https://github.com/datakaveri/forest-stack-developer-docs/blog/',
-        },
+        blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
 
-
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      hideOnScroll: true,
-
-      // Announcement bar as the “Connect / CTA” banner
-      announcementBar: {
-        id: 'department_header',
-        content: 'FOREST DEPARTMENT OF RAJASTHAN',
-        backgroundColor: '#1f2937',
-        textColor: '#FFFFFF',
-        isCloseable: false,
-      },
-
       navbar: {
-        title: '',
+        title: "",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/Forest_Stack.png',
-          href: 'https://forest-stack.digivan.forest.rajasthan.gov.in/',
-          target: '_blank',
+          alt: "Forest Stack Logo",
+          src: "img/Forest_Stack.png",
+          href: "https://forest-stack.digivan.forest.rajasthan.gov.in/",
+          target: "_blank",
         },
         items: [],
       },
-
-
-
       footer: {
-        style: 'light',
+        style: "light",
         links: [
-          // 1st column: logo + description + social icons
+          // 1. Contact Info
           {
+            title: "Forest Stack | Rajasthan",
             items: [
               {
                 html: `
-  <div class="footer__brand">
-    <a href="https://uat.catalogue.forestdx.iudx.io/" target="_blank" rel="noopener">
-      <img
-        src="${baseUrl}img/tgdex-green.png"
-        alt="Forest Stack Logo"
-        style="height:40px; margin-bottom:1px;"
-      />
-    </a>
-    <p class="footer__text">
-        Forest Stack is a digital ecosystem, combining data, technology & governance to enhance forest cover, biodiversity conservation, enhance community involvement and unlock further innovation by startups, academia & conservationists. It is designed to bring a holistic view across multiple forest-related verticals and enable rapid digital interventions in the forestry space.    </p>
-    <h5 class="footer__social-heading">Social Media</h5>
-    <div class="footer__social-icons">
-      <a href="https://www.facebook.com/forestdepartmentrajasthan" target="_blank" rel="noopener">
-        <img src="${baseUrl}img/meta.svg" alt="Meta" />
-      </a>
-      <a href="https://x.com/ForestRajasthan" target="_blank" rel="noopener">
-        <img src="${baseUrl}img/x.svg" alt="X" />
-      </a>
-      <a href="https://www.instagram.com/rajasthanforestdepartment/" target="_blank" rel="noopener">
-        <img src="${baseUrl}img/instagram.svg" alt="Instagram" />
-      </a>
-    </div>
-  </div>`,
+                  <div class="footer__contact-info">
+                    <div class="contact-row">
+                      <img src="${baseUrl}img/location.svg" alt="Loc" />
+                      <a 
+                      href="https://maps.app.goo.gl/Esbv8jMrY1ZkQLbn7" 
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      >
+                      <span>Aranya Bhawan, Mahatma Gandhi Rd, Jhalana Institutional Area, Jaipur, Rajasthan 302004</span>
+                    </a>
+                      </div>
+                    <div class="contact-row">
+                      <img src="${baseUrl}img/global.svg" alt="Web" />
+                      <a href="https://forest.rajasthan.gov.in/" target="_blank">forest.rajasthan.gov.in</a>
+                    </div>
+                    <div class="contact-row">
+                      <img src="${baseUrl}img/sms.svg" alt="Mail" />
+                      <a href="mailto:dcfta.it.forest@rajasthan.gov.in">dcfta.it.forest@rajasthan.gov.in</a>
+                    </div>
+                  </div>`,
               },
             ],
           },
-
-          // 2nd column: “TGDeX”
+          // 2. Important Links (Routing to Main Platform)
           {
-            title: 'Forest Stack',
+            title: "Important links",
             items: [
-              { label: 'Home', to: 'https://forest-stack.digivan.forest.rajasthan.gov.in/' },
-              { label: 'About Forest Stack', to: 'https://forest-stack.digivan.forest.rajasthan.gov.in/about-us' },
-              { label: 'Contact Us', to: 'https://helpdesk.forest-stack.digivan.forest.rajasthan.gov.in/' },
+              {
+                label: "Forest Stack Home",
+                to: "https://forest-stack.digivan.forest.rajasthan.gov.in/",
+              },
+              {
+                label: "About Us",
+                to: "https://forest-stack.digivan.forest.rajasthan.gov.in/about-us",
+              },
+              {
+                label: "Datasets",
+                to: "https://forest-stack.digivan.forest.rajasthan.gov.in/datasets",
+              },
+              {
+                label: "Models",
+                to: "https://forest-stack.digivan.forest.rajasthan.gov.in/models",
+              },
+              {
+                label: "Use Cases",
+                to: "https://forest-stack.digivan.forest.rajasthan.gov.in/usecases",
+              },
+              {
+                label: "Challenges",
+                to: "https://forest-stack.digivan.forest.rajasthan.gov.in/innovations",
+              },
             ],
           },
-
-          // 3rd column: “Platform Features”
+          // 3. Documentation (Split Headers)
           {
-            title: 'Platform Features',
+            title: " ",
             items: [
-              { label: 'Data Sets', to: 'https://forest-stack.digivan.forest.rajasthan.gov.in/datasets' },
-              { label: 'Models', to: 'https://forest-stack.digivan.forest.rajasthan.gov.in/models' },
-              { label: 'Usecases', to: 'https://forest-stack.digivan.forest.rajasthan.gov.in/usecases' },
-              { label: 'Challenges', to: 'https://forest-stack.digivan.forest.rajasthan.gov.in/innovations' },
+              {
+                html: `
+                  <div class="doc-group">
+                    <span class="doc-group-title">Control Plane Documentation</span>
+                    <a href="https://staging.controlplane.forestdx.iudx.io/apis" target="_blank" class="doc-link">Catalogue AAA API Docs</a>
+                  </div>
+                  <div class="doc-group">
+                    <span class="doc-group-title">Data Plane Documentation</span>
+                    <a href="https://staging.rs.forestdx.iudx.io/apis" target="_blank" class="doc-link">Resource Server API Docs</a>
+                    <a href="https://staging.file.forestdx.iudx.io/apis" target="_blank" class="doc-link">File Server API Docs</a>
+                    <a href="https://staging.ogc-s3.forestdx.iudx.io/api" target="_blank" class="doc-link">OGC Server API Docs</a>
+                    <a href="https://staging.rs.forestdx.iudx.io/rsp/apis" target="_blank" class="doc-link">Gateway Server API Docs</a>
+                    <a href="https://voc.forest-stack.iudx.io/" target="_blank" class="doc-link">Vocabulary Docs</a>
+                  </div>`,
+              },
             ],
           },
-
-          // 4th column: “Help & Support”
+          // 4. Help (Internal Docs Routing)
           {
-            title: 'Help & Support',
+            title: "Help & Support",
             items: [
-              { label: 'Help & Support', to: 'https://helpdesk.forest-stack.digivan.forest.rajasthan.gov.in/' },
-              { label: 'FAQs', to: 'https://forest-stack.digivan.forest.rajasthan.gov.in/faq' },
-              { label: 'User Manual', to: '/' },
+              {
+                label: "Help & Support",
+                to: "https://helpdesk.forest-stack.digivan.forest.rajasthan.gov.in/",
+              },
+              {
+                label: "FAQs",
+                to: "https://forest-stack.digivan.forest.rajasthan.gov.in/faq",
+              },
+              { label: "User Manual", to: "/" }, // Points to this Docusaurus site root
+            ],
+          },
+          // 5. Partners
+          {
+            title: "Our Partners",
+            items: [
+              {
+                html: `
+                  <div class="footer__partners">
+                    <div class="partner-row-1">
+                      <img src="${baseUrl}img/iisc-logo-white.svg" class="partner-logo" alt="IISc" />
+                      <img src="${baseUrl}img/jica-logo.svg" class="partner-logo" alt="JICA" />
+                      <img src="${baseUrl}img/doit-logo.svg" class="partner-logo" alt="DOIT" />
+                    </div>
+                    <div class="partner-gov">
+                      <img src="${baseUrl}img/emblem-logo.svg" class="emblem-logo" alt="Gov" />
+                      <div class="gov-text">
+                        <span>Government of Rajasthan</span>
+                        <span>Forest Department</span>
+                      </div>
+                    </div>
+                  </div>`,
+              },
             ],
           },
         ],
+        // Bottom Bar
         copyright: `
-    <hr style="width:100%; margin:2rem 0; border:none; border-top:1px solid #ddd;" />
-<footer>
-  <!-- Flex row: Powered By + Privacy/Terms -->
-  <div class="footer__bottom-content">
-    <div class="footer__powered-by">
-      <span>Powered By:</span>
-      <img src="${baseUrl}img/emerging-tech.svg"
-           alt="Emerging Technologies Logo"
-           style="height:45px;" />
-    </div>
-
-    <div class="footer__privacy-terms">
-      <a href="https://forest-stack.digivan.forest.rajasthan.gov.in/privacy-policy"
-         target="_blank"
-         rel="noopener">
-        Privacy Policy
-      </a>
-      <span>|</span>
-      <a href="https://forest-stack.digivan.forest.rajasthan.gov.in/terms-of-service"
-         target="_blank"
-         rel="noopener">
-        Terms of Service
-      </a>
-    </div>
-  </div>
-
-  <!-- Full-width © strip -->
-  <div class="full-width-cta">
-    &copy; Government of Rajasthan
-  </div>
-</footer>
-
-  `,
+          <div class="footer__bottom-wrapper">
+            <div class="footer__bottom-left">
+              <div class="footer__policy-links">
+                <a href="https://forest-stack.digivan.forest.rajasthan.gov.in/privacy-policy">Privacy Policy</a>
+                <span>|</span>
+                <a href="https://forest-stack.digivan.forest.rajasthan.gov.in/terms-of-service">Terms of Service</a>
+              </div>
+              <div class="footer__copyright">
+                © ${new Date().getFullYear()} by Forest Stack
+              </div>
+            </div>
+            <div class="footer__bottom-right">
+              <span class="connect-title">Connect with us</span>
+              <div class="social-icons">
+                <a href="https://x.com/ForestRajasthan" target="_blank"><img src="${baseUrl}img/twitter.svg" alt="X" /></a>
+                <a href="https://www.facebook.com/forestdepartmentrajasthan" target="_blank"><img src="${baseUrl}img/facebook.svg" alt="FB" /></a>
+                <a href="http://instagram.com/rajasthanforestdepartment" target="_blank"><img src="${baseUrl}img/instagram.svg" alt="Insta" /></a>
+              </div>
+            </div>
+          </div>
+        `,
       },
-
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
-
-      stylesheets: [
-        // ensure your custom footer CSS is loaded
-        '${baseUrl}css/footer.css',
-      ],
     }),
 };
 
